e1f7363941c845fb0928394e7bc6ab91efa0b8d1
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-13 14:30:44 -0400

    get the duration of a video

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 //To create a new browser window and show content to user
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+//listen to the event videoSubmited
+//then callback function to excute whenever
+//the videoSubmited received from main window
+ipcMain.on('videoSubmited', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('video length is : ', metadata.format.duration);
+  });
 });
