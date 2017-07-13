fb2a4a2c08e1382cb5e7282898895e8a0b98ad44
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-13 14:41:06 -0400

    communication between js and html
    how Electron connect html and js together

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const ffmpeg = require('fluent-ffmpeg');
 //To create a new browser window and show content to user
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 //listen to the event videoSubmited
 //then callback function to excute whenever
 //the videoSubmited received from main window
+
+//then send processed information back to main window
 ipcMain.on('videoSubmited', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log('video length is : ', metadata.format.duration);
+    mainWindow.webContents.send('videoMetadata',
+     metadata.format.duration);
   });
 });
